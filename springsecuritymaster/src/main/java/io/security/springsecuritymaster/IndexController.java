5b02e4cb5136d2a776d84802e86ca47fe27ceb3a
tjddn <tjddn3927@naver.com>
2024-06-30 18:31:22 +0900

    form로그인 방식 설정

---
+++ springsecuritymaster/src/main/java/io/security/springsecuritymaster/IndexController.java
@@ -0,0 +1,24 @@
+package io.security.springsecuritymaster;
+
+import lombok.Getter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/loginPage")
+    public String loginPage(){
+        return "loginPage";
+    }
+
+    @GetMapping("/home")
+    public String home(){
+        return "home";
+    }
+}
--- springsecuritymaster/src/main/java/io/security/springsecuritymaster/config/SecurityConfig.java
+++ springsecuritymaster/src/main/java/io/security/springsecuritymaster/config/SecurityConfig.java
@@ -2,9 +2,12 @@ package io.security.springsecuritymaster.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -14,7 +17,33 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults());
+                .formLogin(form -> form
+                        .loginPage("/loginPage")
+                        .loginProcessingUrl("/loginProc")
+                        .defaultSuccessUrl("/",true)
+                        .failureForwardUrl("/failed")
+                        .usernameParameter("userId")
+                        .passwordParameter("password")
+                        .successHandler((request,response,authentication) -> {
+                            System.out.println("authentication: " + authentication);
+                            response.sendRedirect("/home");
+                        })
+                        .failureHandler(((request, response, exception) -> {
+                            System.out.println("exception: " + exception.getMessage());
+                            response.sendRedirect("/login");
+                        }))
+                        .permitAll()
+                );
+
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user = User.withUsername("user")
+                .password("{noop}1111")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
     }
 }
