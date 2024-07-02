c86fb56d784004e363139382f3179146ab34fcd1
tjddn <tjddn3927@naver.com>
2024-07-02 23:23:51 +0900

    익명 사용자 실습

--- springsecuritymaster/src/main/java/io/security/springsecuritymaster/IndexController.java
+++ springsecuritymaster/src/main/java/io/security/springsecuritymaster/IndexController.java
@@ -1,8 +1,14 @@
 package io.security.springsecuritymaster;
 
 import lombok.Getter;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Security;
 
 @RestController
 public class IndexController {
@@ -20,5 +26,25 @@ public class IndexController {
     @GetMapping("/home")
     public String home(){
         return "home";
+    }
+
+    @GetMapping("/anonymous")
+    public String anonymous(){
+        // 익명 이용자만 해당 API에 접근 가능하도록 config에서 설정한 것이다.
+        return "anonymous";
+    }
+
+    @GetMapping("/authentication")
+    public String authentication(Authentication authentication){
+        if(authentication instanceof AnonymousAuthenticationToken){
+            return "anonymous";
+        } else{
+            return "null";
+        }
+    }
+
+    @GetMapping("/anonymousContext")
+    public String anonymousContext(@CurrentSecurityContext SecurityContext securityContext){
+        return securityContext.getAuthentication().getName();
     }
 }
--- springsecuritymaster/src/main/java/io/security/springsecuritymaster/config/form/login/FormLoginSecurityConfig.java
+++ springsecuritymaster/src/main/java/io/security/springsecuritymaster/config/form/login/FormLoginSecurityConfig.java
@@ -16,31 +16,38 @@ public class FormLoginSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .formLogin(form -> form
-                        .loginPage("/loginPage")
-                        .loginProcessingUrl("/loginProc")
-                        .defaultSuccessUrl("/",true)
-                        .failureForwardUrl("/failed")
-                        .usernameParameter("userId")
-                        .passwordParameter("password")
-                        .successHandler((request,response,authentication) -> {
-                            System.out.println("authentication: " + authentication);
-                            response.sendRedirect("/home");
-                        })
-                        .failureHandler(((request, response, exception) -> {
-                            System.out.println("exception: " + exception.getMessage());
-                            response.sendRedirect("/login");
-                        }))
-                        .permitAll()
-                )
-                .rememberMe(rememberMe -> rememberMe
-                        .alwaysRemember(true) // or false 기본 값은 false이다.
-                        .tokenValiditySeconds(3600)
-                        .userDetailsService(userDetailsService())
-                        .rememberMeParameter("remember")
-                        .rememberMeCookieDomain("remember")
-                        .key("security")
+        http.authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/anonymous").hasRole("GUEST") // 익명 객체만 접근 가능하도록 설정도 가능하다.
+                        .requestMatchers("/anonymousContext","/authentication").permitAll() // 익명 객체만 접근 가능하도록 설정도 가능하다.
+                        .anyRequest().authenticated())
+//                .formLogin(form -> form
+//                        .loginPage("/loginPage")
+//                        .loginProcessingUrl("/loginProc")
+//                        .defaultSuccessUrl("/",true)
+//                        .failureForwardUrl("/failed")
+//                        .usernameParameter("userId")
+//                        .passwordParameter("password")
+//                        .successHandler((request,response,authentication) -> {
+//                            System.out.println("authentication: " + authentication);
+//                            response.sendRedirect("/home");
+//                        })
+//                        .failureHandler(((request, response, exception) -> {
+//                            System.out.println("exception: " + exception.getMessage());
+//                            response.sendRedirect("/login");
+//                        }))
+//                        .permitAll()
+//                )
+//                .rememberMe(rememberMe -> rememberMe
+//                        .alwaysRemember(true) // or false 기본 값은 false이다.
+//                        .tokenValiditySeconds(3600)
+//                        .userDetailsService(userDetailsService())
+//                        .rememberMeParameter("remember")
+//                        .rememberMeCookieDomain("remember")
+//                        .key("security")
+//                )
+                .anonymous(anonymous -> anonymous
+                        .principal("guest") // 해당 메서드는 보통 사용자 정보가 들어가는데, 여기서는 익명 사용자의 이름을 담는다. / default 값은 anonymousUser 이다.
+                        .authorities("ROLE_GUEST") // 익명 사용자를 위한 권한
                 );
 
         return http.build();
