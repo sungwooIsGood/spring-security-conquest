491c82b8cdb509b2230a696c115c48b48aa0ff1a
tjddn <tjddn3927@naver.com>
2024-07-30 21:35:49 +0900

    정적 자원 허용

--- spring-security-master-master/src/main/java/io/security/springsecuritymaster/security/configs/SecurityConfig.java
+++ spring-security-master-master/src/main/java/io/security/springsecuritymaster/security/configs/SecurityConfig.java
@@ -15,17 +15,19 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
-    /**
-     * requestMatchers().permitAll(): 를 통해 root 경로는 전체 이용자 허용
-     * anyRequest().authenticated(): 모든 경로는 인증이 필요하다고 설정
-     */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        /**
+         * csrf 기능은 자동적으로 활성 상태가 된다. 수동으로 끄지 않는 이상
+         * post 방식의 요청은 csrf 기능이 켜져있으면 csrf 토큰이 서버가 전달된다.
+         */
         http.authorizeHttpRequests(auth -> auth
-                .requestMatchers("/").permitAll()
-                .anyRequest().authenticated()
+                .requestMatchers("/css/**","/images/**","/js/**","/favicon.*","/*/icon-*").permitAll() // 정적 자원 설정
+                .requestMatchers("/").permitAll() // root 경로는 전체 이용자 허용
+                .anyRequest().authenticated() // 모든 경로는 인증이 필요하다고 설정
         )
-        .formLogin(Customizer.withDefaults());
+        .formLogin(Customizer.withDefaults());// 커스텀 로그인 페이지 경로 설정, 모든 이용자 접근 가능
 
         return http.build();
     }
